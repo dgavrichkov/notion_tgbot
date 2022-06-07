957e07f2cd70df92f4fbd85151d9985cf455eb96
dmitriy <dgavrichkov32@gmail.com>
2022-06-07 17:13:08 +0300

    set some packages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,8 +2,16 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
+  // const commandMatch = message.text.match(/(?<=\/).*?(?=$| |@)/);
+  // const command = commandMatch ? commandMatch[0] : null;
+  // const botNameMatch = message.text.match(/(?<=@).*?(?=($| ))/);
+  // const botName = botNameMatch ? botNameMatch[0] : null;
+  // const extraMatch = message.text.match(/(?<=\s).*?(?=$)/);
+  // const extra = extraMatch ? extraMatch[0] : null;
 
-  await sendMessage(message.chat.id, "stand by");
+  if (botName === "DitmarNotionNotes_bot" || botName === null) {
+    await sendMessage(message.chat.id, "I got your message!");
+  }
 
   return { statusCode: 200 };
 };
