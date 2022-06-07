7a6fe827ad6fb916e274248189059f7ded0943f5
dmitriy <dgavrichkov32@gmail.com>
2022-06-07 16:03:24 +0300

    refactor sending message

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,16 +1,8 @@
-const axios = require("axios").default;
-
-const { NOTION_KEY, NOTION_DB_KEY, BOT_KEY } = process.env;
-const TG_API = `https://api.telegram.org/bot${BOT_KEY}/sendMessage`;
+const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body);
-  console.log("NOTION_KEY", NOTION_KEY);
-
-  await axios.post(TG_API, {
-    chat_id: JSON.parse(event.body).message.chat.id,
-    text: "I got your message!",
-  });
+  const { message } = JSON.parse(event.body);
+  await sendMessage(message.chat_id, "I got the message");
 
   return { statusCode: 200 };
 };
