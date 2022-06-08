7697ddd72a863f724914aeb98abb8e221d59ad49
dmitriy <dgavrichkov32@gmail.com>
2022-06-08 15:44:08 +0300

    add hasnode answer

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,15 +1,27 @@
 const sendMessage = require("../../sendMessage");
 const messageParts = require("../../messageParts");
+const hashnode = require("../../hashnode");
+const { BOT_NAME } = require("../../helpers/consts");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   console.log(event);
   const { command, botName, extra } = messageParts(message.text);
 
-  if (botName === "DitmarNotionNotes_bot" || botName === null) {
+  if (botName === BOT_NAME || botName === null) {
     switch (command) {
       case "echo":
         await sendMessage(message.chat.id, extra || "ECHO!");
+        break;
+      case "hashnodefeatured":
+        const { storiesFeed } = await hashnode.getFeaturedPosts();
+        const reply = `
+          ${storiesFeed[0].title} by ${storiesFeed[0].author.username}
+          ${storiesFeed[1].title} by ${storiesFeed[1].author.username}
+          ${storiesFeed[2].title} by ${storiesFeed[2].author.username}
+          https://hashnode.com/featured
+        `;
+        await sendMessage(message.chat.id, reply);
         break;
       default:
         await sendMessage(message.chat.id, "I don't understand that command.");
